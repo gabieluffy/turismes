92e4a5356701674a93d14fe88c3f8b337e57046e
sergio <sergioluand@gmail.com>
2026-05-17 17:58:57 -0300

    :bug: fix deploy3: configurando a porta parao render

--- flask/run.py
+++ flask/run.py
@@ -2,6 +2,7 @@
 
 from app import create_app
 from app.extensions import db
+import os
 
 app = create_app()
 
@@ -9,4 +10,12 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(
+        os.environ.get("PORT", 5000)
+    )
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
